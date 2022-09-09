0eb3e98c8e3b5888b85213a78254db3a824474e5
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-09-09 10:24:34 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,105 @@
+class Script(object):
+    START_TXT = """<i><b>Hello 👋 {} {},</i></b>
+
+<i><b>I Am Not Only <a href="https://t.me/Moviesclub_Robot">Movie Club Bot</a> To Assist You But Also Employed At <a href="https://t.me/+iVePgxkxvQ0yOTBl">Movie Club</a> Group By <a href="https://t.me/hellodragan">Dragon</a> So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data</i></b> 😉
+
+<i><b>Better You Click Below & Join <a href="https://t.me/+iVePgxkxvQ0yOTBl">Movie Club</a> & Feel The Experience Of Downloading Unlimited Movies/Series</i></b> ✅
+
+<i><b>For More Information Click ℹ️ Help</i></b>"""
+    
+    HELP_TXT = """<i><b>Hello 👋 {},</i></b>
+
+<i><b>I can Guide You Through All Of <a href="https://t.me/MoviesClub_Robot">Movie Club Bot</a>'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules.</i></b>"""
+
+    ALIVE_TXT = """<b>Module :-</b> Alive
+
+- To Find Out If I'm Alive Or Not
+
+<b>📚 Available Command:</b>
+
+• /alive - Look
+
+<b>Maintained By ✅ <a href="https://t.me/hellodragan">Dragon</a></b>"""
+
+    LINK_TXT = """<b>Module :-</b> Invite Link
+
+- You Can Easily Find Group Invitation Link
+
+<b>📚 Available Command:</b>
+
+• /link - To Get Movie Club Group Link
+
+<b>Maintained By ✅ <a href="https://t.me/hellodragan">Dragon</a></b>"""
+
+    INFO_TXT = """<b>Module :-</b> IDs
+
+- A Module To Fetch Telegram User, Group, Channel Info
+
+<b>📚 Available Commands:</b>
+• /id - get id of a specified user.
+• /info  - get information about a user.
+• /json - get the json details of a message.
+
+<b>Maintained By ✅ <a href="https://t.me/hellodragan">Dragon</a></b>""" 
+
+    SEARCH_TXT = """<b>Module :-</b> Imdb Info
+
+- A Module To Get The Movie Informations. Use This Module To Get Movie Informations
+
+<b>📚 Available Commands:</b>
+• /imdb  - get the film information from IMDb source.
+• /search  - get the film information from various sources.
+
+<b>Maintained By ✅ <a href="https://t.me/hellodragan">Dragon</a></b>""" 
+  
+    STATUS_TXT = """<b>Total Files:</b> <code>{}</code>
+<b>Total Users:</b> <code>{}</code>
+<b>Total Chats:</b> <code>{}</code>
+<b>Used Storage:</b> <code>{}</code> MiB
+<b>Free Storage:</b> <code>{}</code> MiB"""
+
+    FORCESUB_TXT = """**👋 Hello**
+
+**You Have Not Subscribed To My Channel.** 
+
+**To View The File, Click On 📣 Join Updates Channel Button & Join.** 
+
+**Then Click On The 🔄 Try Again Button To Receive The File ✅**"""
+
+    MUSIC_TXT = """<b>Module :-</b> Song
+
+- Music download modules, for those who love music.
+
+<b>📚 Available Commands:</b>
+• /song or /mp3 (songname) - download song from yt servers.
+• /video or /mp4 (songname) - download video from yt servers.
+
+<b>Maintained By ✅ <a href="https://t.me/hellodragan">Dragon</a></b>""" 
+  
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+
+    LOG_TEXT_P = """#NewUser
+🆔 - <code>{}</code>
+Name - {}"""
+
+    CREATOR_REQUIRED = """❗You have to be the group creator to do that."""
+      
+    INPUT_REQUIRED = "❗ **Arguments Required**"
+      
+    KICKED = """✔️ Successfully Kicked {} members according to the arguments provided."""
+      
+    START_KICK = """🚮 Removing inactive members this may take a while..."""
+      
+    ADMIN_REQUIRED = """❗I am not an admin here\n__Leaving this chat, add me again as admin with ban user permission."""
+      
+    DKICK = """✔️ Kicked {} Deleted Accounts Successfully."""
+      
+    FETCHING_INFO = """Collecting users information..."""
+      
+    STATUS = """{}\nChat Member Status**\n\n```recently``` - {}\n```within_week``` - {}\n```within_month``` - {}\n```long_time_ago``` - {}\nDeleted Account - {}\nBot - {}\nUnCached - {}
+"""
+
