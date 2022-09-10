6bcfdef390092fa4240f0909295813ca8a8152c3
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-09-10 09:39:02 +0530

    Update gtranslator.py

--- plugins/privateproject/gtranslator.py
+++ plugins/privateproject/gtranslator.py
@@ -28,15 +28,26 @@ async def left(client,message):
 			tr_text = message.reply_to_message.text
 			translator = Translator()
 			translation = translator.translate(tr_text,dest = lg_cd)
-	                for i in list:
-		              if list[i]==translation.src:
-				 fromt = i
-		              if list[i] == translation.dest:
-			         to = i 
+	                hehek = InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton(
+                                            text=f"translated from {translation.src} to {translation.dest}", url="https://cloud.google.com/translate/docs/languages"
+                                        )
+                                    ],
+                                ]
+                            )
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
 				await message.reply_text(f"```{translation.text}```", reply_markup=hehek, reply_to_message_id=message.reply_to_message.message_id)
 			except:
 			   	await message.reply_text(f"```{translation.text}```", reply_markup=hehek, reply_to_message_id=message.reply_to_message.message_id)
 			
+
 
 		except :
 			print("error")
